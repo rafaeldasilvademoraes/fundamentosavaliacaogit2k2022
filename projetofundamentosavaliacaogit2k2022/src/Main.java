bcce03debf0d45eab53946c3b48053ba2ebc3f73
"rafaeldasilvademoraes2104@gmail.com" <alunoinfo@lab02m23>
2023-02-23 08:37:52 -0300

    Removido erros

--- projetofundamentosavaliacaogit2k2022/src/Main.java
+++ projetofundamentosavaliacaogit2k2022/src/Main.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Olá, mundo!");
-<<<<<<< HEAD
 		System.out.println("Olá, dev!");
-=======
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Digite o seu nome:");
 		String nome = scanner.next();
 		System.out.println("Olá, " + nome + "!");
 		scanner.close();
->>>>>>> refs/heads/funcionalidade
 	}
 }
