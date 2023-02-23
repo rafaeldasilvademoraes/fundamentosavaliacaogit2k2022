ec395f8314683e68591529987e33d67f8c7408f7
"rafaeldasilvademoraes2104@gmail.com" <alunoinfo@lab02m23>
2023-02-23 08:30:58 -0300

    Adicionado funcionalidade digitar o nome e imprimir natela

--- projetofundamentosavaliacaogit2k2022/src/Main.java
+++ projetofundamentosavaliacaogit2k2022/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Olá, mundo!");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o seu nome:");
+		String nome = scanner.next();
+		System.out.println("Olá, " + nome + "!");
+		scanner.close();
 	}
 }
